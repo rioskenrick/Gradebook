36e429d3d26a3a4f878f26026acc3b2a2a1ddff0
rioskenrick <142235372+rioskenrick@users.noreply.github.com>
2023-10-19 16:56:40 -0500

    Delete JAVA directory

--- JAVA/java.js
+++
@@ -1,5 +0,0 @@
-function calc() {
-let first = (document.getElementById("fname").value);
-let last = (document.getElementById("lname").value)
-let fullname = first + last; document.getElementById("full").innerHTML = fullname ;
-}
