ce2655a4ac9e16ecef9374b46d73d65ab80166c7
rioskenrick <142235372+rioskenrick@users.noreply.github.com>
2023-10-19 16:14:33 -0500

    Add files via upload

---
+++ JAVA/java.js
@@ -0,0 +1,5 @@
+function calc() {
+let first = (document.getElementById("fname").value);
+let last = (document.getElementById("lname").value)
+let fullname = first + last; document.getElementById("full").innerHTML = fullname ;
+}
